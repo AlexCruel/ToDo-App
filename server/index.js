08e438802b7fab152e7d15f885f41a561604fb69
Alex <sasha.karpenkov@outlook.com>
2020-01-01 16:10:21 +0300

    trying

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
-const posts = require('./routes/api/posts');
-
 app.use('/api/posts', posts);
 
 // Handle production
